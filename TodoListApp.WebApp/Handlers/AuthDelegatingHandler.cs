40636b74f67c72db9e7c83248a0942109abbb8d6
Ruslan-Vladyslav <rgritsai95@gmail.com>
2026-06-12 08:46:02 +0300

    add email service for sending emails via SMTP; authdelegate for attaching jwt tokens to http requests

---
+++ TodoListApp.WebApp/Handlers/AuthDelegatingHandler.cs
@@ -0,0 +1,27 @@
+using System.Net.Http.Headers;
+
+public class AuthDelegatingHandler : DelegatingHandler
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public AuthDelegatingHandler(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        var token = _httpContextAccessor.HttpContext?
+            .Request.Cookies["jwt"];
+
+        if (!string.IsNullOrEmpty(token))
+        {
+            request.Headers.Authorization =
+                new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
